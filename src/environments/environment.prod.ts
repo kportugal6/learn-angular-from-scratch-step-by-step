f95c730d7c6fa62cfacb4522f152fa23d0befbd9
Karina <kportugal96@gmail.com>
2025-02-22 16:31:47 -0800

    URL updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  api_url: 'https://4b0xxibsej.execute-api.us-east-1.amazonaws.com'
+  api_url: 'https://j6bzb1zb30.execute-api.us-east-1.amazonaws.com'
+  
 };
